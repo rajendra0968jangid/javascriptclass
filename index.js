7d2632593eccb1f34dbf808f65493067cfc3b07a
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-06-08 11:52:05 +0530

    basic js

---
+++ index.js
@@ -0,0 +1,90 @@
+// //var
+// var num = 10;
+// {
+//     num = 20;
+// }
+// console.log("var",num)
+
+// // let
+// let num1 = 10
+
+// {
+//     num1 = 20
+// }
+// console.log("let",num1)
+
+// //const
+// const num2 = 10
+
+// {
+//     num2 = "helo"
+// }
+// console.log("const",num2)
+
+
+// var num = 10
+// {
+//     var num = 20
+//     num = 30
+//     console.log(num)
+// }
+// console.log(num)
+
+// let num1 = 10
+// {
+//     let num1 = 20
+//     num1 = 30
+//     console.log(num1)
+// }
+// console.log(num1)
+
+// const num2 = 10
+// {
+//     const num2 = 20
+//     console.log(num2)
+// }
+// console.log(num2);
+
+//reassign
+// var num = 10
+// {
+//     var num = 20
+//     num = 30
+//     console.log(num)
+// }
+// console.log(num)
+
+// let num1 = 10
+// {
+//     let num1 = 20
+//     num1 = 30
+//     console.log(num1)
+// }
+// console.log(num1)
+
+// const num2 = 10
+// {
+//     const num2 = 20
+//     console.log(num2)
+// }
+// console.log(num2);
+
+
+// let num1 = 5;
+// let num2 = 10;
+// let sum = --num1 + ++num2 + num1-- + num2++
+// console.log(sum)
+// console.log(num1)
+// console.log(num2);
+
+// let a = 10
+// let b = 20;
+// let c = 30
+// let result = (a < b && a < c) ? "a" : (b < c) ? "b" : "c";
+// console.log(result);
+
+let num1 = {"key":"val"}
+let arr = [1,2,3]
+
+let result = ( num1 &&  num2) ? "yes" : "no";
+console.log(result);
