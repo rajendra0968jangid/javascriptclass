2dcfeee6e15fee175f2997d4773f0e16d45c5752
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-06-10 11:33:37 +0530

    function object array

--- index.js
+++ index.js
@@ -83,8 +83,80 @@
 // let result = (a < b && a < c) ? "a" : (b < c) ? "b" : "c";
 // console.log(result);
 
-let num1 = {"key":"val"}
-let arr = [1,2,3]
+// let num1 = {"key":"val"}
+// let arr = [1,2,3]
 
-let result = ( num1 &&  num2) ? "yes" : "no";
-console.log(result);
+// let result = ( num1 &&  num2) ? "yes" : "no";
+// console.log(result);
+
+
+// function add() {
+//     console.log("hello")
+// }
+
+// add();
+
+// function add(a) { //para 
+//     console.log(a) 
+// }
+// add(10); //arg
+// add(); //arg
+
+
+// function add(...a) { //para 
+//     console.log(a) 
+// }
+// add(10,{"key":"val"},30); //arg
+
+// function add(b,...a){
+//     console.log(b);
+// }
+
+// add("10","20",{"key":"val"})
+
+// function add(a){
+//     return a + a;
+// }
+
+// const result = add("2");
+// console.log(result)
+
+
+
+// const result =  (a,b)=> {
+//     return b;
+// }
+// console.log(result(10,20));
+
+
+// (()=>{
+//     console.log("hello");
+// })()
+
+
+// let str = "hello is my name";
+
+// console.log(str[3])
+// console.log(str.slice(0,6));
+// console.log(str.length);
+
+// let num = "120"
+// console.log(typeof parseInt(num));
+
+// const obj = {
+//     "key": "val",
+//     "first name": "raj",
+//     age: function () {
+//         return "age"
+//     }
+// }
+// console.log(obj["key"])
+// console.log(obj["first name"])
+// console.log(obj.age());
+
+const arr = ["hello", "one", { "firstname": "helo", "secondName": "surname" }, [120, 140, [3, 2, 1]]]
+
+// console.log(arr[2]["firstname"])
+//console.log(arr[3][2])
+console.log(arr[3][2][2])
+
