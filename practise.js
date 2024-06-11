fd2ff87fe9a465cc46daa001051a34740b2b385f
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-06-11 11:59:51 +0530

    loop

---
+++ practise.js
@@ -0,0 +1,87 @@
+// let age = 10;
+// console.log(age);
+
+// let name1 = 'it"s hello';
+// console.log(name1);
+
+// let isCheck = true
+// console.log(isCheck);
+
+// let person = {"key":10,"str":"helo","bool":true,"fun":function (){
+//     return "obj"
+// }}
+// console.log(person["key"]);
+// console.log(person["str"]);
+// console.log(person["bool"]);
+// console.log(person.fun());
+
+
+// let arr = ["helo",10]
+
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+// function add(num){
+//     // console.log("my pocket money is "+(num+num)+" in current time");
+//     console.log(`my pocket money is ${num+num} in current time`);
+// }
+// add(100)
+
+// function add(num){
+//     return num;
+// }
+// const result = add(100);
+// console.log(result);
+
+// let age = 18
+// if (age == 18) {
+//     console.log("greater then 18");
+// }
+// else if (age == 18) {
+//     console.log("equal to 18");
+// }
+// else {
+//     console.log("less then 18");
+// }
+
+// let str = "etvar";
+// switch (str) {
+//     case "monday":
+//         console.log("today is monday");
+//         break;
+//     case "tuesday":
+//         console.log("today is tuesday");
+//         break;
+//     case "wednesday":
+//         console.log("today is wednesday");
+//         break;
+//     default:
+//         console.log("this is hindi weak day");
+// }
+
+//loop
+
+// let i = 5;
+// while (i > 0) {
+//     console.log("helo");
+//     --i;
+// }
+
+// let i = 1;
+// do {
+//     console.log(2*i);
+//     i++
+// } while (i <= 10);
+
+// let i;
+// let table = 3
+// for (i = 1; i <= 10; i++) {
+//     console.log(`${table}X${i}=${table * i}`);
+// }
+
+// let date = new Date();
+// console.log(date.getFullYear());
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getMonth());
+
